ac6618da124a1c1fed369b6d50c468530ef4b0f1
Aryan <114349788+AryanSwami18@users.noreply.github.com>
2023-08-16 23:44:59 +0530

    added user

--- Main.java
+++ Main.java
@@ -1,33 +1,111 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to Quiz APP");
-        
-        // QuizBrain initialization
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Quiz App");
+
+        User currentUser = null;
+
+        while (true) {
+            System.out.println("\nMenu:");
+            if (currentUser == null) {
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+            } else {
+                System.out.println("Logged in as: " + currentUser.getUsername());
+                System.out.println("3. Play Quiz");
+                System.out.println("4.Logout");
+            }
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            String  choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    if (currentUser == null) {
+                        System.out.print("Enter a username: ");
+                        String username = scanner.nextLine();
+                        System.out.print("Enter a password: ");
+                        String password = scanner.nextLine();
+                        currentUser = new User(username, password);
+                        System.out.println("Registration successful!");
+                        currentUser.setIsloggedIn();
+                    } else {
+                        System.out.println("You are already logged in.");
+                    }
+                    break;
+
+                case "2":
+                    if (currentUser == null) {
+                        System.out.print("Enter your username: ");
+                        String username = scanner.nextLine();
+                        System.out.print("Enter your password: ");
+                        String password = scanner.nextLine();
+                        currentUser = new User(username, password);
+                        currentUser.login(username, password);
+                        if (currentUser.isLoggedIn()) {
+                            System.out.println("Login successful!");
+                        } else {
+                            System.out.println("Login failed. Invalid username or password.");
+                            currentUser = null; 
+                        }
+                    } else {
+                        System.out.println("You are already logged in.");
+                    }
+                    break;
+
+                case "3":
+                    if (currentUser != null && currentUser.isLoggedIn()) {
+                        playQuiz(scanner);
+                    } else {
+                        System.out.println("You must be logged in to play the quiz.");
+                    }
+                    break;
+                
+                    
+                case "4":
+                    if (currentUser != null && currentUser.isLoggedIn()) {
+                        currentUser.logout();
+                        currentUser = null;
+                        System.out.println("Logout successful.");
+                    } else {
+                        System.out.println("You are not logged in.");
+                    }
+                    break;
+
+                case "5":
+                    System.out.println("Exiting the program.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public static void playQuiz(Scanner scanner) {
         QuizBrain quizBrain = new QuizBrain();
-        
-        // Quiz Start Here
+
         System.out.println("Quiz Starts NOW!!!!");
         System.out.println("You have a total of 5 Questions. Each question has 1 point");
-        
+
         int start = 0;
         while (start < 5) {
             System.out.println("Question " + (start + 1) + ": " + quizBrain.fetchQuestion());
-            String userAnswer = s.nextLine();
-            
+            String userAnswer = scanner.nextLine();
+
             if (quizBrain.checkAnswer(userAnswer)) {
                 System.out.println("Correct!");
             } else {
                 System.out.println("Incorrect. The correct answer is: " + quizBrain.getCorrectAnswer());
             }
-            
+
             start++;
             quizBrain.nextQuestion();
         }
-        
-        // Quiz End here
-        s.close();
     }
 }
--- User.java
+++ User.java
@@ -1,3 +1,35 @@
 public class User {
-    
+    private String username;
+    private String password;
+    private boolean isLoggedIn;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.isLoggedIn = false;
+    }
+
+    public boolean login(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            isLoggedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setIsloggedIn(){
+        isLoggedIn = true;
+    }
+
+    public void logout(){
+        this.isLoggedIn = false;
+    }
 }
